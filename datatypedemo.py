afebbb727dd5f661f0cf8c69e94a6500616aad4c
rashmisingh5033 <rashmisingh5033@gmail.com>
2023-08-25 19:14:02 +0530

    add some new python file

--- datatypedemo.py
+++ datatypedemo.py
@@ -6,3 +6,10 @@ print("octal to decimal=",int(n1))
 print("binary to dicimal=",int(n2))
 print("hexadeciaml to deciaml=",int(n3))
 
+n1= 15
+n2= 46
+n3= 450
+
+print("decimal to octal=",oct(n1))
+print("decimal to binary=",bin(n2))
+print("deciaml to hexadecial=",hex(n3))
---
+++ stringdatatype.py
@@ -0,0 +1,7 @@
+s="welcome to python world"
+print(s)
+print(s[0])
+print(s[0:7])
+print(s[-1])
+print(s[::])
+print(s[::2])
