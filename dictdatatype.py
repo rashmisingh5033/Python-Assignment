94198a77445f3c0c715fab1636b2f2b64d2bb2d6
rashmisingh5033 <rashmisingh5033@gmail.com>
2023-08-26 19:58:55 +0530

    add set and dict datatype program

---
+++ dictdatatype.py
@@ -0,0 +1,7 @@
+dict={101:"sham",102:"rashmi",103:"manoj",104:"nilam",105:"shivam"}
+
+print(dict)
+dict[106]="priya"
+print(dict)
+dict[101]="priti"
+print(dict)
---
+++ setdatatype.py
@@ -0,0 +1,10 @@
+s={10,20,30,40,50,60,80}
+print(s)
+
+ch=set("hello")
+print(ch)
+
+s.update([95])
+s.remove(10)
+print(s)
+print(type(s))
