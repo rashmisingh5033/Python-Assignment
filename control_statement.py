9a211194141bdf5176169c421fb0ae6529703204
rashmisingh5033 <rashmisingh5033@gmail.com>
2023-08-28 19:58:19 +0530

    small python programs

---
+++ control_statement.py
@@ -0,0 +1,27 @@
+import sys
+a=int(input("enter any number between 1 to 5\n"))
+if a==1:
+    print("one")
+elif a==2:
+    print("two")
+elif a==3:
+    print("three")
+elif a==4:
+    print("four")
+else:
+    print("five")
+
+
+sys.exit()
+a=10
+b=20
+if(a==10):
+    print("x")
+    print("y")
+    print("z")
+    if(b==20):
+        print("p")
+        print("q")
+
+print("end of the program")
+
---
+++ operators.py
@@ -0,0 +1,16 @@
+a=10
+b=20
+print("Example of arithmetic operator")
+print("addition",a+b)
+print("subtraction",a-b)
+print("multiplication",a*b)
+print("division",a/b)
+print("modulo",a%b)
+print("exponent",a**b)
+print("integar division",a//b)
+
+print("\nexample of assignment operator")
+print("assignment equal",a=b)
+print("addition assignment", a=a+b)
+
+
